8e532c1b33cca2ce723b35e0f9842cf54f708647
sergiioss <sergiiptx@gmail.com>
2023-11-27 13:25:37 +0100

    comenzando conecta 4

---
+++ index.js
@@ -0,0 +1,10 @@
+
+let pantallaInicio = document.getElementById("pantallaInicio");
+let start = document.getElementById("start");
+let conecta4 = document.getElementById("conecta4");
+
+function empezar(){
+    pantallaInicio.style = "display:none";
+    start.style = "display:none";
+    conecta4.style = "display:flex";
+}
